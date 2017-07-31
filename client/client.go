3878124a4221838d9c888048de8de10d9412d629
xiaoziqingren <1045977704@qq.com>
2017-07-31 20:56:24 +0800

    实现github的API调用
    实现github的API 调用

---
+++ client/client.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
+	githuboauth "golang.org/x/oauth2/github"
+)
+
+var (
+	oauthConf = &oauth2.Config{
+		ClientID:     "Iv1.c4ed7a603c7d18fa",
+		ClientSecret: "c4bdf10018d1fd68e466151a42385502b0a12220",
+		Scopes:       []string{"user:email", "repo"},
+		Endpoint:     githuboauth.Endpoint,
+	}
+
+	oauthStateString = "littlestar"
+)
+
+const htmlIndex = `<html><body>
+Logged in with <a href="/login">GitHub</a>
+</body></html>
+`
+
+func handleMain(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(htmlIndex))
+}
+
+func handleGitHubLogin(w http.ResponseWriter, r *http.Request) {
+	url := oauthConf.AuthCodeURL(oauthStateString, oauth2.AccessTypeOnline)
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+}
+
+func handleGitHubCallback(w http.ResponseWriter, r *http.Request) {
+	state := r.FormValue("state")
+	if state != oauthStateString {
+		fmt.Printf("invalid oauth state, expected '%s', got '%s'\n", oauthStateString, state)
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	code := r.FormValue("code")
+
+	token, err := oauthConf.Exchange(oauth2.NoContext, code)
+	if err != nil {
+		fmt.Printf("oauthConf.Exchange() failed with '%s'\n", err)
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	oauthClient := oauthConf.Client(oauth2.NoContext, token)
+	client := github.NewClient(oauthClient)
+
+	user, _, err := client.Users.Get(context.Background(), "")
+	if err != nil {
+		fmt.Printf("client.Users.Get() faled with '%s'\n", err)
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+	fmt.Printf("Logged in as GitHub user: %s\n", *user.Login)
+	http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+}
+
+func main() {
+	http.HandleFunc("/", handleMain)
+	http.HandleFunc("/login", handleGitHubLogin)
+	http.HandleFunc("/github_oauth_cb", handleGitHubCallback)
+	fmt.Print("Started running on http://127.0.0.1:7000\n")
+	fmt.Println(http.ListenAndServe(":7000", nil))
+}
+
+//func tokenToJSON(token *oauth2.Token) (string, error) {
+//	if d, err := json.Marshal(token); err != nil {
+//		return "", err
+//	} else {
+//		return string(d), nil
+//	}
+//}
+//
+//func tokenFromJSON(jsonStr string) (*oauth2.Token, error) {
+//	var token oauth2.Token
+//	if err := json.Unmarshal([]byte(jsonStr), &token); err != nil {
+//		return nil, err
+//	}
+//	return &token, nil
+//}
